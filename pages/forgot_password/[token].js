fa02f521ec3d79ce418ad49ac16ad3074d6885e7
annashrul <anashrulyusuf@gmail.com>
2022-07-22 22:28:48 +0700

    fix

--- pages/forgot_password/[token].js
+++
@@ -1,5 +0,0 @@
-// import VerifyForgotPasswordComponent from "../../components/auth/VerifyForgotPasswordComponent";
-
-// const VerifyForgotPasswordPage = () => <VerifyForgotPasswordComponent />;
-
-// export default VerifyForgotPasswordPage;
